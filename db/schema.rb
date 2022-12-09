00e3805305446d41f8449ed340f326c40055a96d
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-12-09 15:20:49 +0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,38 +14,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_155308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "appointments", force: :cascade do |t|
-    t.integer "patient_id"
-    t.integer "practitioner_id"
-    t.string "date"
-    t.string "duration"
-    t.string "appointment_type"
+  create_table "fruits", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "fruit_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "caregivers", force: :cascade do |t|
-    t.string "name"
-    t.string "phone"
-    t.string "relation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ 
 
-  create_table "patients", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "condition"
-    t.integer "caregiver_id"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "practitioners", force: :cascade do |t|
-    t.string "name"
-    t.string "speciality"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  
 
 end
